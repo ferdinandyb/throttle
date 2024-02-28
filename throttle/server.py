e3d01d5d159196cc72fe39cf900b35189a2dddb1
Bence Ferdinandy <bence@ferdinandy.com>
2024-02-28 14:18:37 +0100

    server: added cleanups and throttling

--- throttle/server.py
+++ throttle/server.py
@@ -2,6 +2,8 @@ from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 import socket
 from multiprocessing import Process, Queue
 from pathlib import Path
+from dataclasses import dataclass
+import queue
 import time
 import subprocess
 
@@ -15,10 +17,18 @@ def ipcworker(socketpath, handleMsg):
     srv.serve_forever()
 
 
+@dataclass
+class workeritem:
+    p: Process
+    q: Queue
+    t: float
+
+
 class MessageWorker:
     def __init__(self, queue):
         self.q = queue
         self.data = {}
+        self.timeout = 20
 
     def msgworker(self):
         while True:
@@ -26,22 +36,50 @@ class MessageWorker:
             msg = self.q.get()
             curtime = time.time()
             print("worker", curtime, msg)
-            self.executeworker(msg)
+            key = " ".join(msg)
+            if key not in self.data or not self.data[key].p.is_alive():
+                print(f"{key}: doesn't exist or finished, creating")
+                q = Queue()
+                p = Process(
+                    target=self.workerFactory(),
+                    args=(q, self.timeout),
+                )
+                p.start()
+                self.data[key] = workeritem(p, q, time.time())
+            print(f"{key}: approx queue size {self.data[key].q.qsize()}")
+            if self.data[key].q.empty():
+                print(f"{key}: empty, adding new")
+                self.data[key].q.put(msg)
+            self.data[key].t = time.time()
 
-    def executeworker(self, msg):
-        def runner(args):
-            print("started")
-            proc = subprocess.Popen(args)
-            proc.wait()
-            print("finished in runner")
-            self.finished()
+            self.cleanup()
 
-        p = Process(target=runner, args=(msg,))
-        p.start()
-        return p
+    def workerFactory(self):
+        def worker(q, timeout):
+            while True:
+                try:
+                    msg = q.get(timeout=timeout)
+                    print("starting", time.time(), msg)
+                    proc = subprocess.Popen(msg)
+                    proc.wait()
+                    print("finished", time.time(), msg)
+                except queue.Empty:
+                    print("finishing")
+                    break
 
-    def finished(self):
-        print("done")
+        return worker
+
+    def cleanup(self):
+        # go through process and queues that haven't been touch in X minutes
+        print("cleanup underway", self.data.keys())
+        toclean = []
+        for key, val in self.data.items():
+            if not val.p.is_alive():
+                toclean.append(key)
+
+        for key in toclean:
+            del self.data[key]
+        print("cleanup finished", self.data.keys())
 
 
 def start_server(socketpath):
