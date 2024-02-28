6b5b587e60b717609d4da6626bb6380b1e8f8566
Bence Ferdinandy <bence@ferdinandy.com>
2024-02-28 11:34:10 +0100

    server: scaffolding

--- throttle/server.py
+++ throttle/server.py
@@ -1,7 +1,9 @@
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 import socket
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Queue
 from pathlib import Path
+import time
+import subprocess
 
 
 def ipcworker(socketpath, handleMsg):
@@ -13,26 +15,49 @@ def ipcworker(socketpath, handleMsg):
     srv.serve_forever()
 
 
-def msgworker(q):
-    while True:
-        msg = q.get()
-        print("worker", msg)
+class MessageWorker:
+    def __init__(self, queue):
+        self.q = queue
+        self.data = {}
+
+    def msgworker(self):
+        while True:
+            print("restarting loop")
+            msg = self.q.get()
+            curtime = time.time()
+            print("worker", curtime, msg)
+            self.executeworker(msg)
+
+    def executeworker(self, msg):
+        def runner(args):
+            print("started")
+            proc = subprocess.Popen(args)
+            proc.wait()
+            print("finished in runner")
+            self.finished()
+
+        p = Process(target=runner, args=(msg,))
+        p.start()
+        return p
+
+    def finished(self):
+        print("done")
 
 
 def start_server(socketpath):
-    with Manager() as manager:
-        queue = manager.Queue()
+    queue = Queue()
 
-        def handleMsg(msg):
-            queue.put(msg)
+    def handleMsg(msg):
+        queue.put(msg)
 
-        p_ipc = Process(
-            target=ipcworker,
-            args=(socketpath, handleMsg),
-        )
-        p_msg = Process(target=msgworker, args=(queue,))
+    p_ipc = Process(
+        target=ipcworker,
+        args=(socketpath, handleMsg),
+    )
+    msgworker = MessageWorker(queue)
+    p_msg = Process(target=msgworker.msgworker, args=())
 
-        p_ipc.start()
-        p_msg.start()
-        p_ipc.join()
-        p_msg.join()
+    p_ipc.start()
+    p_msg.start()
+    p_ipc.join()
+    p_msg.join()
